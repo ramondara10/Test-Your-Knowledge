7aa4c69c9f92f860d2e6591640c86c52da49d421
Ramon Alvarado <ramon_alvarado96@hotmail.com>
2021-03-06 16:08:08 -0600

    work ready for review

--- assets/questionScript.js
+++ assets/questionScript.js
@@ -1,29 +1,28 @@
 // make your questions
 
-var testQuestion = [
+var questions = [
     {
         title: "1) To make your website mobile friendly, you can make your website",
         choices: ["responsive", "reactive", "fast loading", "light"],
         answer: "responsive",
-    }
+    },
     {
         title: "2) What does CSS stands for",
         choices: ["Current Style Sense", "Cascading Style Sheets", "Cascade Super Sheet", "Cascading Sheet of Styles"],
         answer: "Cascading Style Sheets",
-    }
+    },
     {
         title: "3) What does API stand for",
         choices:["Amazon Prime Internship", "App Processing Intermission", "Application Process Intense", "Application Programming Interface"],
         answer: "Application Programming Interface",
-    }
+    },
     {
         title: "4) What does JSON.stringify does?",
         choices: ["Converts Javascript obejcts into strings", "Converts Javascript obejcts into booleans", "Converts Javascript obejcts into a function", "Converts Javascript obejcts into variables"],
         answer: "Converts Javascript obejcts into strings",
-    }
+    },
     {
         title: "5) Which one of the following is a HTML semantic",
         choices: ["<time>", "<div>", "<p>", "<body>"],
         answer: "<time>",
-    }
-}]
+    }]
